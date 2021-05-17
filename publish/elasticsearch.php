de6357e5f9c5b01e73869535501d43d0bddf7b04
huangdijia <huangdijia@gmail.com>
2021-05-17 11:40:25 +0800

    add ConfigProvider

---
+++ publish/elasticsearch.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of elasticsearch.
+ *
+ * @link     https://github.com/friendsofhyperf/elasticsearch
+ * @document https://github.com/friendsofhyperf/elasticsearch/blob/main/README.md
+ * @contact  hdj@addcn.com
+ */
+return [
+    'hosts' => [
+        'http://127.0.0.1:9500',
+    ],
+];
---
+++ src/ConfigProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of elasticsearch.
+ *
+ * @link     https://github.com/friendsofhyperf/elasticsearch
+ * @document https://github.com/friendsofhyperf/elasticsearch/blob/main/README.md
+ * @contact  hdj@addcn.com
+ */
+namespace FriendsOfHyperf\Elasticsearch;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        defined('BASE_PATH') or define('BASE_PATH', '');
+
+        return [
+            'dependencies' => [],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+            'commands' => [],
+            'listeners' => [],
+            'publish' => [
+                [
+                    'id' => 'config',
+                    'description' => 'config file of elasticsearch.',
+                    'source' => __DIR__ . '/../publish/elasticsearch.php',
+                    'destination' => BASE_PATH . '/config/autoload/elasticsearch.php',
+                ],
+            ],
+        ];
+    }
+}
