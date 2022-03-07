ce36d6391c838e147f5245336c4c019824fec2c3
D.J.Hwang <huangdijia@gmail.com>
2022-03-07 12:31:14 +0800

    Fixed

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -10,33 +10,13 @@ declare(strict_types=1);
  */
 namespace FriendsOfHyperf\Elasticsearch;
 
-use FriendsOfHyperf\Elasticsearch\Exception\InvalidClientProxyException;
-use Hyperf\Contract\ConfigInterface;
-
 class ClientFactory
 {
-    /**
-     * @var ClientProxy[]
-     */
-    protected $proxies;
-
-    public function __construct(ConfigInterface $config)
-    {
-        foreach ($config->get('elasticsearch') as $poolName => $configure) {
-            $this->proxies[$poolName] = make(ClientProxy::class, ['pool' => $poolName]);
-        }
-    }
-
     /**
      * @return ClientProxy
      */
     public function get(string $poolName)
     {
-        $proxy = $this->proxies[$poolName] ?? null;
-        if (! $proxy instanceof ClientProxy) {
-            throw new InvalidClientProxyException('Invalid Client proxy.');
-        }
-
-        return $proxy;
+        return make(ClientProxy::class, ['pool' => $poolName]);
     }
 }
